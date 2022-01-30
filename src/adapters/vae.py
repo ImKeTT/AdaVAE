fc63967aaedf7909082e0a8a969bbab2f40054f5
ImKeTT <thq415_ic@yeah.net>
2022-01-30 21:23:28 +0800

    add code book for EQ-VAE like model

--- src/adapters/vae.py
+++ src/adapters/vae.py
@@ -1207,6 +1207,48 @@ class LM_head_rep(nn.Module):
         return z
 
 
+## For VQ-VAE like big AE
+class CodeBook(nn.Module):
+    def __init__(self, num_embeddings, embedding_dim, commitment_cost):
+        """
+        Code Book for VQ-VAE
+        :param num_embeddings:
+        :param embedding_dim:
+        :param commitment_cost:
+        """
+        super(CodeBook, self).__init__()
+        self._embedding_dim = embedding_dim
+        self._num_embeddings = num_embeddings
+        self._embedding = nn.Embedding(self._num_embeddings, self._embedding_dim)
+        self._commitment_cost = commitment_cost
+
+    def forward(self, inputs):
+        # Calculate distances
+        distances = (torch.sum(inputs ** 2, dim=1, keepdim=True)
+                     + torch.sum(self._embedding.weight ** 2, dim=1)
+                     - 2 * torch.matmul(inputs, self._embedding.weight.t()))
+
+        # Encoding
+        encoding_indices = torch.argmin(distances, dim=1).unsqueeze(1)
+        encodings = torch.zeros(encoding_indices.shape[0], self._num_embeddings).cuda()
+        encodings.scatter_(1, encoding_indices, 1)
+
+        # Quantize and unflatten
+        quantized = torch.matmul(encodings, self._embedding.weight)
+
+        # Loss
+        e_latent_loss = torch.mean((quantized.detach() - inputs) ** 2)
+        q_latent_loss = torch.mean((quantized - inputs.detach()) ** 2)
+        loss = q_latent_loss + self._commitment_cost * e_latent_loss
+
+        quantized = inputs + (quantized - inputs).detach()
+        avg_probs = torch.mean(encodings, dim=0)
+        perplexity = torch.exp(-torch.sum(avg_probs * torch.log(avg_probs + 1e-10)))
+
+        # convert quantized from BHWC -> BCHW
+        return loss, quantized, perplexity, encodings
+
+
 class VAEModel(GPT2LMHeadModel):
     def __init__(self, config, AdapterConfig, add_input=False, add_attn=False, add_softmax=False,
                  attn_proj_vary=False, learn_prior=False, label_cond=False):

e37c95f96fad6d862dcf5883fca07318f1947c48
ImKeTT <thq415_ic@yeah.net>
2022-03-06 11:08:57 +0800

    add cyclic annealing for KLD

--- src/utils.py
+++ src/utils.py
@@ -214,6 +214,20 @@ def linear_schedule(args):
     return f
 
 
+def frange_cycle_linear(n_iter, start=0.0, stop=1.0,  n_cycle=4, ratio=0.5):
+    L = np.ones(n_iter) * stop
+    period = n_iter / n_cycle
+    step = (stop-start)/(period*ratio) # linear schedule
+
+    for c in range(n_cycle):
+        v, i = start, 0
+        while v <= stop and (int(i+c*period) < n_iter):
+            L[int(i+c*period)] = v
+            v += step
+            i += 1
+    return L
+
+
 def compare_date(date1: str, date2: str)->bool:
     """
     whether date2 is no earlier than date1
